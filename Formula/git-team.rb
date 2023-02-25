282b8557aa27fa6d040cd5d58b42344ca643b734
Rea Sand <hekmek@posteo.de>
2023-02-25 12:51:46 +0100

    refactor: use ruby 1.9 hash syntax

--- Formula/git-team.rb
+++ Formula/git-team.rb
@@ -2,10 +2,10 @@ class GitTeam < Formula
   desc "Manage and enhance `git commit` messages with co-authors"
   homepage "https://github.com/hekmekk/git-team"
   url "https://github.com/hekmekk/git-team.git",
-      :tag      => "v1.8.1",
-      :revision => "22e2334998a03346cb5ad0c4cc6f4788af4ef181"
+      tag:      "v1.8.1",
+      revision: "22e2334998a03346cb5ad0c4cc6f4788af4ef181"
   license "MIT"
-  head "https://github.com/hekmekk/git-team.git", :branch => "main"
+  head "https://github.com/hekmekk/git-team.git", branch: "main"
 
   depends_on "go@1.18" => :build
   depends_on "git"
