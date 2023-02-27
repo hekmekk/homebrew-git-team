10244fca84c29773cecd8376679947c03176044d
Rea Sand <hekmek@posteo.de>
2023-02-27 17:00:40 +0100

    refactor: use go 1.19 for building

--- Formula/git-team.rb
+++ Formula/git-team.rb
@@ -7,7 +7,7 @@ class GitTeam < Formula
   license "MIT"
   head "https://github.com/hekmekk/git-team.git", branch: "main"
 
-  depends_on "go@1.18" => :build
+  depends_on "go@1.19" => :build
   depends_on "git"
 
   def install
